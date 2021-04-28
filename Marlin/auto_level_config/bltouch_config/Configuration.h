d5d6bfee7a00a3729fca561d6285068c628c265e
shaneaw <51542213+shaneaw@users.noreply.github.com>
2021-04-28 18:53:33 -0400

    Update z offset

--- Marlin/auto_level_config/bltouch_config/Configuration.h
+++ Marlin/auto_level_config/bltouch_config/Configuration.h
@@ -930,7 +930,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { 40, 10, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { 40, 10, -1.550 }
 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 10
